782da6f33fdf2a7c1d0a15c8b795d8f94845c4f6
mkdirYannick <yannick.pell@gmail.com>
2022-01-07 10:37:19 +0100

    Alternate between X and O, but the game doesn't test for a winner yet

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.getElementById('container');
 let squares = document.getElementsByClassName('square');
 let arraySquares = Array.from(squares);
 let clear = document.getElementById('clear');
+let player1Turn = true;
+let player2Turn = false;
 
 const game = {
     gameboard: [
@@ -18,14 +20,26 @@ const game = {
     },
 }
 
-arraySquares.forEach(element => {
-    element.addEventListener('click', function() {
-        if (squares[element.dataset.id].textContent == '') {
-        game.gameboard[element.dataset.id].push(element.dataset.id);
-        squares[element.dataset.id].textContent = game.gameboard[element.dataset.id];
-        }
+function playGame() {
+    arraySquares.forEach(element => {
+        element.addEventListener('click', function() {
+            if (squares[element.dataset.id].textContent == '')
+                if (player1Turn) {
+                game.gameboard[element.dataset.id].push('O');
+                squares[element.dataset.id].textContent = game.gameboard[element.dataset.id];
+                player1Turn = false;
+                player2Turn = true;
+                } else if (player2Turn) {
+                    game.gameboard[element.dataset.id].push('X');
+                    squares[element.dataset.id].textContent = game.gameboard[element.dataset.id];
+                    player1Turn = true;
+                    player2Turn = false; 
+                }
+            });
     });
-});
+}
+
+playGame();
 
 clear.onclick = () => {
     arraySquares.forEach(element => {
